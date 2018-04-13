496da4791ce441956da5b48ab00a57b658df03e1
elenapalombini <elena.palombini@users.noreply.github.com>
2018-04-13 11:51:19 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/project/carryon/carryon/HomeActivity.java

--- app/src/main/java/com/project/carryon/carryon/HomeActivity.java
+++ app/src/main/java/com/project/carryon/carryon/HomeActivity.java
@@ -9,7 +9,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        int i = 1;
-        String s = "Hello World";
     }
 }
