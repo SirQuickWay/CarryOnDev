b609db1b08111b63450a40066df5be9123a43729
matteodemartis <matteo.demartis.b51s@gmail.com>
2018-04-15 22:28:44 +0800

    Login activity linked with the server.

---
+++ app/src/main/java/com/project/carryon/carryon/GeneralClasses/User.java
@@ -0,0 +1,45 @@
+package com.project.carryon.carryon.GeneralClasses;
+
+
+/**
+ * Created by matte on 15/04/2018.
+ */
+
+public class User {
+    private String UID;
+    private String name;
+    private String surname;
+    private String username;
+    private String email;
+
+    public User(String UID, String name, String surname, String username, String email) {
+        this.UID = UID;
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+//others fields
+
+}
--- app/src/main/java/com/project/carryon/carryon/LoginActivity.java
+++ app/src/main/java/com/project/carryon/carryon/LoginActivity.java
@@ -1,13 +1,71 @@
 package com.project.carryon.carryon;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        Button loginButton = findViewById(R.id.button_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginButtonOnClick();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+            Toast.makeText(LoginActivity.this, "User authenticated!",Toast.LENGTH_SHORT).show();
+
+    }
+    private void loginButtonOnClick()
+    {
+        EditText emailEditText = findViewById(R.id.editText_email);
+        EditText passwordEditText = findViewById(R.id.editText_password);
+        loginUser(emailEditText.getText().toString(), passwordEditText.getText().toString());
+    }
+    private void loginUser(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, get signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "User authenticated!",Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails,
+                            Toast.makeText(LoginActivity.this, "Email or password incorrect!",Toast.LENGTH_SHORT).show();
+                            EditText passwordEditText = findViewById(R.id.editText_password);
+                            passwordEditText.setText("");
+                        }
+
+
+                    }
+                });
     }
 }
