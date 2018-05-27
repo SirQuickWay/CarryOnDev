e8e13b8bc397b5f42a55aa78fd0a996348e23252
elenapalombini <elena.palombini@users.noreply.github.com>
2018-05-27 11:57:20 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/project/carryon/carryon/HomeActivity.java

--- app/src/main/java/com/project/carryon/carryon/HomeActivity.java
+++ app/src/main/java/com/project/carryon/carryon/HomeActivity.java
@@ -1,11 +1,30 @@
 package com.project.carryon.carryon;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TabHost;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.project.carryon.carryon.GeneralClasses.Delivery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String currentUID; //GET FROM AUTH
+
+    //INITIALIZE LISTVIEW STUFF
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +51,54 @@ public class HomeActivity extends AppCompatActivity {
         spec.setContent(R.id.tab3);
         spec.setIndicator("All Paths");
         host.addTab(spec);
+
+        final List<Delivery> userDeliveries = new ArrayList<>(); //List for MyOrders
+
+        db.collection("deliveries")
+                .whereEqualTo("senderID","currentUID") //also add deliveries where current user is user2
+                //.whereGreaterThanOrEqualTo -- time comparison within deliveries
+                //orderBy("time").limit(3); -- sample code for ordering deliveries / or maybe we can order it later
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Delivery delivery= document.toObject(Delivery.class);
+                                //Toast.makeText(getApplicationContext(), "delivery retrieved", Toast.LENGTH_SHORT ).show();
+                                userDeliveries.add(delivery);
+                            }
+                            //pathAdapter = new PathAdapter(getApplicationContext(), paths); ADAPTERS GO HERE
+                            //pathListView.setAdapter(pathAdapter);             CODE FROM TEST APP
+                        } else {
+                            Log.d(TAG, "Error getting deliveries: ", task.getException());
+                        }
+                    }
+                });
+
+        db.collection("deliveries")                   //this is to add deliveries where current user is user2
+                .whereEqualTo("receiverID","currentUID")
+                //.whereGreaterThanOrEqualTo -- time comparison within deliveries
+                //orderBy("time").limit(3); -- sample code for ordering deliveries (order by time)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Delivery delivery= document.toObject(Delivery.class);
+                                //Toast.makeText(getApplicationContext(), "delivery retrieved", Toast.LENGTH_SHORT ).show();
+                                userDeliveries.add(delivery);
+                            }
+                            //pathAdapter = new PathAdapter(getApplicationContext(), paths); ADAPTERS GO HERE
+                            //pathListView.setAdapter(pathAdapter);             CODE FROM TEST APP
+                        } else {
+                            Log.d(TAG, "Error getting deliveries: ", task.getException());
+                        }
+                    }
+                });
 
     }
 }
