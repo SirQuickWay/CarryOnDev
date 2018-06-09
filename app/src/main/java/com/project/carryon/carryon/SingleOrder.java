16ccc9e94091e0274e9942ed58153a44e3b2985a
elenapalombini <elena.palombini@users.noreply.github.com>
2018-06-09 13:29:51 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/project/carryon/carryon/HomeActivity.java

--- app/src/main/java/com/project/carryon/carryon/SingleOrder.java
+++ app/src/main/java/com/project/carryon/carryon/SingleOrder.java
@@ -9,5 +9,7 @@ public class SingleOrder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_order);
+
+
     }
 }
